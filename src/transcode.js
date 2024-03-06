3e5930908d5767cf8ff4712873902f87f661f416
Damião Junior Terto <contato@damiaoterto.dev>
2024-03-06 15:34:30 -0300

    feat: create transcoder function

---
+++ src/transcode.js
@@ -0,0 +1,39 @@
+const { createReadStream } = require('node:fs')
+const { spawn } = require('node:child_process')
+
+const transcode = (path) => {
+  const ffmpegArgs = [
+    '-i', 'pipe:0',
+    '-f', 'mp4',
+    '-vcodec', 'h264',
+    '-acodec', 'aac',
+    '-movflags', 'frag_keyframe+empty_moov+default_base_moof',
+    '-b:v', '1500K',
+    '-maxrate', '1500K',
+    '-bufsize', '1000K',
+    '-f', 'mp4',
+    'pipe:1',
+  ]
+
+  const ffmpegProcess = spawn('ffmpeg', ffmpegArgs, {
+    stdio: ['pipe', 'pipe', 'pipe']
+  })
+
+  createReadStream(path).pipe(ffmpegProcess.stdin)
+
+  ffmpegProcess.stderr.on('data', (msg) => console.error(msg.toString()));
+
+  const stream = () => ffmpegProcess.stdout
+  const close = () => {
+    ffmpegProcess.stdin.destroy()
+    ffmpegProcess.stdout.destroy()
+    ffmpegProcess.kill()
+  }
+
+  return {
+    stream,
+    close,
+  }
+}
+
+module.exports = { transcode }
